59e857283751ec66c02dc03af0b87ad452f5e88f
MAHESH VUNNAM <MY_NAME@example.com>
2025-03-27 00:45:31 +0530

    added themecontext to auto detect the system theme and set the theme

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,20 @@ import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
+import ThemeProvider from "./components/ThemeProvider"; // Import ThemeProvider
+
 function App() {
   return (
-    <div className="App font-sans bg-gray-50 dark:bg-gray-800 text-gray-900 dark:text-gray-100">
-      <Navbar />
-
-      <Hero />
-
-      <About />
-
-      <Skills />
-
-      <Projects />
-
-      <Contact />
-    </div>
+    <ThemeProvider> {/* Wrap everything inside ThemeProvider */}
+      <div className="App font-sans bg-gray-50 dark:bg-gray-800 text-gray-900 dark:text-gray-100">
+        <Navbar />
+        <Hero />
+        <About />
+        <Skills />
+        <Projects />
+        <Contact />
+      </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/ThemeProvider.jsx
@@ -0,0 +1,30 @@
+import { createContext, useEffect, useState, useContext } from "react";
+
+const ThemeContext = createContext();
+
+export function useTheme() {
+  return useContext(ThemeContext);
+}
+
+export default function ThemeProvider({ children }) {
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "dark" ? "light" : "dark"));
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
